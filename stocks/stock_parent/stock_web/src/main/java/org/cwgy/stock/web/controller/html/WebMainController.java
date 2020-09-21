a4959a74584c4cdfa1a42300bfbe2a55821c8922
willie.chen <bcskcnuj@hotmail.com>
2020-09-21 19:37:49 +0800

    examples_202009211937

--- stocks/stock_parent/stock_web/src/main/java/org/cwgy/stock/web/controller/html/WebMainController.java
+++ stocks/stock_parent/stock_web/src/main/java/org/cwgy/stock/web/controller/html/WebMainController.java
@@ -4,24 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import graphql.schema.GraphQLFieldDefinition;
-import graphql.schema.GraphQLObjectType;
-import graphql.schema.GraphQLObjectType.Builder;
-
 @Controller
 public class WebMainController {
 
 	@RequestMapping(value = "/testError", method = { RequestMethod.GET, RequestMethod.POST })
 	public String mainIndex() {
-		Builder builder = GraphQLObjectType.newObject();
-		builder.name("Foo");
-		
-		GraphQLFieldDefinition.Builder fieldBuilder = GraphQLFieldDefinition.newFieldDefinition();
-		fieldBuilder.name("bar");
-		fieldBuilder.type(graphql.Scalars.GraphQLString);
-		
-		builder.field(fieldBuilder);
-		
 		return "test";
 	}
 }
