1065ef705ee31eb4ccb5ea43b95f538fd9e345ef
willie.chen <bcskcnuj@hotmail.com>
2020-09-23 22:26:33 +0800

    examples_202009232226

--- stocks/stock_parent/stock_core/src/test/java/org/cwgy/stock/core/service/TestTStockService.java
+++ stocks/stock_parent/stock_core/src/test/java/org/cwgy/stock/core/service/TestTStockService.java
@@ -1,16 +1,26 @@
 package org.cwgy.stock.core.service;
 
+import org.cwgy.stock.core.Mapper.TStockExample;
+import org.cwgy.stock.core.dao.TStockMapper;
+import org.cwgy.stock.core.service.impl.TStockServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 class TestTStockService extends TestBaseService{
+	
+	@Mock
+	private TStockMapper stockMapper;
 
-	@Autowired
-	private TStockService stockService;
+	@InjectMocks
+	private TStockServiceImpl stockService;
 
 	@Test
 	void test() {
+		Mockito.when(stockMapper.countByExample(Mockito.isA(TStockExample.class))).thenReturn(1L);
+		
 		Assertions.assertEquals(stockService.getCount(), 1);
 	}
 
